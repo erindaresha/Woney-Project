a957e7aa63becbbfea506efa7eea2896c512121d
Yohanes Chris Kurniawan <yohanes.chris@ti.ukdw.ac.id>
2018-11-13 05:04:52 +0700

    Top Up

--- app/src/main/java/id/ac/ukdw/woney/HomeFragment.java
+++ app/src/main/java/id/ac/ukdw/woney/HomeFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,8 +14,17 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Map;
 
 
 /**
@@ -27,8 +37,11 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
     private TextView txtName, txtUsername, txtSaldo;
     String nama, username;
     float saldo;
-    TextView txtSend;
-    View view;
+    FirebaseDatabase db = FirebaseDatabase.getInstance();
+    DatabaseReference user = db.getReference("user");
+    ArrayList listUser;
+    Locale localeID = new Locale("in", "ID");
+    NumberFormat formatRupiah = NumberFormat.getCurrencyInstance(localeID);
 
     public HomeFragment() {
         // Required empty public constructor
@@ -39,8 +52,8 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         LayoutInflater lf = getActivity().getLayoutInflater();
-        view = lf.inflate(R.layout.fragment_home, container, false);
-        nf = new DecimalFormat("#.###");
+        View view = lf.inflate(R.layout.fragment_home, container, false);
+        //nf = new DecimalFormat("#.###");
         sp = this.getActivity().getSharedPreferences("session", Context.MODE_PRIVATE);
         spEdit = sp.edit();
         txtName = view.findViewById(R.id.txtName);
@@ -48,18 +61,32 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
         txtSaldo = view.findViewById(R.id.txtSaldo);
         txtSend = view.findViewById(R.id.txtSend);
 
+        user.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                nama = sp.getString("nama", null);
+                username = sp.getString("username", null);
+                listUser = (ArrayList) dataSnapshot.getValue();
+                String _saldo="", _username="";
+                for (int i = 0; i < listUser.size(); i++) {
+                    Map mapUser = (Map) listUser.get(i);
+                    _username += mapUser.get("username");
+                    if (username.equals(_username)) {
+                        _saldo += mapUser.get("saldo");
+                        Float f = Float.parseFloat(_saldo);
+                        txtName.setText(nama);
+                        txtUsername.setText(username);
+                        txtSaldo.setText(formatRupiah.format(f));
+                        break;
+                    }
+                }
+            }
 
-        nama = sp.getString("nama", null);
-        username = sp.getString("username", null);
-        saldo = (float)sp.getFloat("saldo", 0);
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
-        txtName.setText(nama);
-        txtUsername.setText(username);
-        String Saldo = "Rp ";
-        Saldo += nf.format((double)saldo) + ",-";
-        txtSaldo.setText(Saldo);
-
-        txtSend.setOnClickListener(this);
+            }
+        });
 
         return view;
     }
