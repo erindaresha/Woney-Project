185b72fe69350b9227db647699072a8a15691b62
Charles Eka Swandi <charles.eka@ti.ukdw.ac.id>
2018-11-13 03:20:45 +0700

    tampilan

--- app/src/main/java/id/ac/ukdw/woney/HomeActivity.java
+++ app/src/main/java/id/ac/ukdw/woney/HomeActivity.java
@@ -1,13 +1,76 @@
 package id.ac.ukdw.woney;
 
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class HomeActivity extends AppCompatActivity {
+    private BottomNavigationView navMainNav;
+    private FrameLayout frmMainFrame;
+
+    private HomeFragment homeFragment;
+    private TransactionFragment transactionFragment;
+    private HistoryFragment historyFragment;
+    private AccountFragment accountFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        frmMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        navMainNav = (BottomNavigationView) findViewById(R.id.main_nav);
+
+        homeFragment = new HomeFragment();
+        transactionFragment = new TransactionFragment();
+        historyFragment = new HistoryFragment();
+        accountFragment = new AccountFragment();
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, homeFragment);
+        fragmentTransaction.commit();
+
+        navMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.nav_home:
+                        navMainNav.setItemBackgroundResource(R.color.colorPrimary);
+                        setFragment(homeFragment);
+                        return true;
+                    case R.id.nav_transaction:
+                        navMainNav.setItemBackgroundResource(R.color.colorPrimary);
+                        setFragment(transactionFragment);
+                        return true;
+
+                    /*case R.id.nav_history:
+                        navMainNav.setItemBackgroundResource(R.color.colorPrimary);
+                        setFragment(historyFragment);
+                        return true;*/
+
+                    case R.id.nav_account:
+                        navMainNav.setItemBackgroundResource(R.color.colorPrimary);
+                        setFragment(accountFragment);
+                        return true;
+
+                    default: return false;
+                }
+            }
+
+            private void setFragment(Fragment fragment) {
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.main_frame, fragment);
+                fragmentTransaction.commit();
+            }
+        });
     }
 }
--- app/src/main/java/id/ac/ukdw/woney/LoginActivity.java
+++ app/src/main/java/id/ac/ukdw/woney/LoginActivity.java
@@ -1,33 +1,141 @@
 package id.ac.ukdw.woney;
 
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Paint;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Map;
+
+public class LoginActivity extends MasterActivity {
 
     private EditText txtUsername;
     private EditText txtPassword;
     private Button btnLogin;
     private TextView txtCreateUser;
+    Context mContext;
+    boolean isLogin;
+    String username;
+    String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
 
         //Inisialisasi
         txtUsername = (EditText) findViewById(R.id.txtUsername);
         txtPassword = (EditText) findViewById(R.id.txtPassword);
         btnLogin = (Button) findViewById(R.id.btnLogin);
         txtCreateUser = (TextView) findViewById(R.id.txtCreateUser);
+        mContext = this;
 
+        //pengecekan apakah user sudah login, dan pengecekan apakah user sudah pernah login atau belum
+        if(sp.getBoolean("isLogin", false)) {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else if(!sp.getBoolean("isLogin", false) && sp.getBoolean("pernahLogin", false)) {
+            Intent intent = new Intent(this, Login2Activity.class);
+            startActivity(intent);
+            finish();
+        }
 
+        //set listener
+        btnLogin.setOnClickListener(this);
+        txtCreateUser.setOnClickListener(this);
+    }
+
+    @Override
+    public int getLayoutResourceID() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnLogin :
+                login();
+                if(isLogin) {
+                    Intent intent = new Intent(this, HomeActivity.class);
+                    startActivity(intent);
+                }
+                break;
+            case R.id.txtCreateUser :
+                daftar();
+                break;
+        }
+    }
+
+    private void login() {
+        username = txtUsername.getText().toString();
+        password = txtPassword.getText().toString();
+        user.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                listUser = (ArrayList) dataSnapshot.getValue();
+                String uname = "", pass = "", email = "", name = "", pin = "", saldo = "";
+                for (int i=0; i<listUser.size(); i++) {
+                    Map mapUser = (Map) listUser.get(i);
+                    uname += mapUser.get("username");
+                    pass += mapUser.get("password");
+                    name += mapUser.get("name");
+                    email += mapUser.get("email");
+                    pin += mapUser.get("pin");
+                    saldo += mapUser.get("saldo");
+                    if (uname.equals(username) && pass.equals(password)) {
+                        isLogin = true;
+                        spEdit.putBoolean("isLogin", true);
+                        spEdit.putBoolean("pernahLogin", true);
+                        spEdit.putString("username", username);
+                        spEdit.putString("nama", name);
+                        spEdit.putString("password", password);
+                        spEdit.putString("pin", pin);
+                        spEdit.putString("email", email);
+                        Float f = Float.parseFloat(saldo);
+                        spEdit.putFloat("saldo", f);
+                        spEdit.apply();
+                        Toast.makeText(mContext, "Login Berhasil", Toast.LENGTH_SHORT).show();
+                        Intent homeIntent = new Intent(mContext, HomeActivity.class);
+                        startActivity(homeIntent);
+                        finish();
+                        break;
+                    } else {
+                        isLogin = false;
+                        Toast.makeText(mContext, "Login Gagal", Toast.LENGTH_SHORT).show();
+                        uname = "";
+                        pass = "";
+                        pin = "";
+                        name = "";
+                        email = "";
+                        saldo = "";
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+    }
+
+    private void daftar() {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
