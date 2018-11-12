d93f2bd71b48355a9b40013ec5ce1045b04570c4
Yohanes Chris Kurniawan <yohanes.chris@ti.ukdw.ac.id>
2018-11-13 01:21:28 +0700

    Bisa log in, log out, baca akun

--- app/src/main/java/id/ac/ukdw/woney/AccountFragment.java
+++ app/src/main/java/id/ac/ukdw/woney/AccountFragment.java
@@ -1,18 +1,33 @@
 package id.ac.ukdw.woney;
 
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AccountFragment extends Fragment {
-
+public class AccountFragment extends Fragment implements View.OnClickListener {
+    NumberFormat nf;
+    SharedPreferences sp;
+    SharedPreferences.Editor spEdit;
+    private TextView txtName, txtUsername, txtSaldo;
+    String nama, username;
+    float saldo;
+    Button btnLogout;
+    View view;
 
     public AccountFragment() {
         // Required empty public constructor
@@ -22,8 +37,40 @@ public class AccountFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false);
+        LayoutInflater lf = getActivity().getLayoutInflater();
+        view = lf.inflate(R.layout.fragment_account, container, false);
+        //nf = new DecimalFormat("#.###");
+        sp = this.getActivity().getSharedPreferences("session", Context.MODE_PRIVATE);
+        spEdit = sp.edit();
+        txtName = view.findViewById(R.id.txtName);
+        txtUsername = view.findViewById(R.id.txtUsername);
+        btnLogout = view.findViewById(R.id.btnLogoutFragmentLogout);
+        //txtSaldo = view.findViewById(R.id.txtSaldo);
+
+        nama = sp.getString("nama", null);
+        username = sp.getString("username", null);
+        //saldo = (float)sp.getFloat("saldo", 0);
+        btnLogout.setOnClickListener(this);
+
+        txtName.setText(nama);
+        txtUsername.setText(username);
+//        String Saldo = "Rp ";
+//        Saldo += nf.format((double)saldo) + ",-";
+//        txtSaldo.setText(Saldo);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnLogoutFragmentLogout :
+                spEdit.clear();
+                spEdit.commit();
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+                break;
+        }
+    }
 }
--- app/src/main/java/id/ac/ukdw/woney/HomeFragment.java
+++ app/src/main/java/id/ac/ukdw/woney/HomeFragment.java
@@ -1,18 +1,29 @@
 package id.ac.ukdw.woney;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
-
+    NumberFormat nf;
+    SharedPreferences sp;
+    SharedPreferences.Editor spEdit;
+    private TextView txtName, txtUsername, txtSaldo;
+    String nama, username;
+    float saldo;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -22,8 +33,26 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        LayoutInflater lf = getActivity().getLayoutInflater();
+        View view = lf.inflate(R.layout.fragment_home, container, false);
+        nf = new DecimalFormat("#.###");
+        sp = this.getActivity().getSharedPreferences("session", Context.MODE_PRIVATE);
+        spEdit = sp.edit();
+        txtName = view.findViewById(R.id.txtName);
+        txtUsername = view.findViewById(R.id.txtUsername);
+        txtSaldo = view.findViewById(R.id.txtSaldo);
+
+        nama = sp.getString("nama", null);
+        username = sp.getString("username", null);
+        saldo = (float)sp.getFloat("saldo", 0);
+
+        txtName.setText(nama);
+        txtUsername.setText(username);
+        String Saldo = "Rp ";
+        Saldo += nf.format((double)saldo) + ",-";
+        txtSaldo.setText(Saldo);
+
+        return view;
     }
 
 }
