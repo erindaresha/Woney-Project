5279b50b0a4c162b4970ea0fac4f1e6d2b3951a2
erindaresha <38478927+erindaresha@users.noreply.github.com>
2018-11-13 09:32:45 +0700

    history update

--- app/src/main/java/id/ac/ukdw/woney/TransactionFragment.java
+++ app/src/main/java/id/ac/ukdw/woney/TransactionFragment.java
@@ -1,17 +1,22 @@
 package id.ac.ukdw.woney;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TransactionFragment extends Fragment {
+public class TransactionFragment extends Fragment{
 
 
     public TransactionFragment() {
@@ -22,8 +27,31 @@ public class TransactionFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transaction, container, false);
-    }
 
+        View view = inflater.inflate(R.layout.fragment_transaction, container, false);
+
+        final String[] items = new String[] { "Transfer", "Transfer", "Transfer",
+                "Top Up", "Top Up", "Menerima", "Menerima", "Top Up",
+                "Transfer", "Transfer" };
+        // Inflate the layout for this fragment
+        final ListView list = (ListView)view.findViewById(R.id.list_view);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, items);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                    long arg3) {
+                // TODO Auto-generated method stub
+                Log.v("TAG", "CLICKED row number: " + arg2);
+                Intent intent = new Intent(getActivity(), HistoryActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+        return view;
+    }
 }
