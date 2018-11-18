b08b43eea6dd88b4d9243bd418e98b608667f49a
Charles Eka Swandi <charles.eka@ti.ukdw.ac.id>
2018-11-18 19:08:00 +0700

    transaksi

---
+++ app/src/main/java/id/ac/ukdw/woney/TransactionAdapter.java
@@ -0,0 +1,80 @@
+package id.ac.ukdw.woney;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.google.firebase.database.Transaction;
+
+import java.util.List;
+
+import mbanje.kurt.fabbutton.CircleImageView;
+
+public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.TransactionHolder> {
+
+    private List<id.ac.ukdw.woney.Transaction> listTransaction;
+    private TransactionFragment mContext;
+
+
+    public TransactionAdapter(List<id.ac.ukdw.woney.Transaction> listTransaction, TransactionFragment mContext) {
+        this.listTransaction = listTransaction;
+        this.mContext = mContext;
+    }
+
+    public class TransactionHolder extends RecyclerView.ViewHolder {
+        private ImageView imgTransaction;
+        private TextView txtForm, txtDate , txtValue;
+
+        public TransactionHolder(View itemView) {
+            super(itemView);
+
+            imgTransaction = (ImageView) itemView.findViewById(R.id.img_item_photo);
+            txtForm = (TextView) itemView.findViewById(R.id.item_from);
+            txtDate = (TextView) itemView.findViewById(R.id.item_date);
+            txtValue = (TextView) itemView.findViewById(R.id.item_value);
+
+        }
+    }
+
+    @Override
+    public TransactionHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_transaction, parent, false);
+
+        return new TransactionHolder(itemView);
+    }
+
+
+
+    @Override
+    public void onBindViewHolder(TransactionHolder holder, int position) {
+        id.ac.ukdw.woney.Transaction transaction = listTransaction.get(position);
+        holder.txtForm.setText(transaction.from);
+        holder.txtDate.setText(transaction.date);
+        if(transaction.status == "kirim"){
+            holder.imgTransaction.setImageResource(R.drawable.red_arrow);
+            holder.txtValue.setTextColor(mContext.getResources().getColor(R.color.red));
+            holder.txtValue.setText("- " + transaction.value + " IDR");
+        }
+        else if(transaction.status == "terima"){
+            holder.imgTransaction.setImageResource(R.drawable.green_arrow);
+            holder.txtValue.setTextColor(mContext.getResources().getColor(R.color.green));
+            holder.txtValue.setText("+ " + transaction.value + " IDR");
+        }
+        else{
+            holder.imgTransaction.setImageResource(R.drawable.blue_circle);
+            holder.txtValue.setTextColor(mContext.getResources().getColor(R.color.blue));
+            holder.txtValue.setText("+ " + transaction.value + " IDR");
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return listTransaction.size();
+    }
+}
--- app/src/main/java/id/ac/ukdw/woney/TransactionFragment.java
+++ app/src/main/java/id/ac/ukdw/woney/TransactionFragment.java
@@ -4,6 +4,10 @@ package id.ac.ukdw.woney;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,17 +16,16 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TransactionFragment extends Fragment{
-
-
-    public TransactionFragment() {
-        // Required empty public constructor
-    }
-
+public class TransactionFragment extends Fragment {
+    private RecyclerView listTransaction;
+    private TransactionAdapter transactionAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,28 +33,25 @@ public class TransactionFragment extends Fragment{
 
         View view = inflater.inflate(R.layout.fragment_transaction, container, false);
 
-        final String[] items = new String[] { "Transfer", "Transfer", "Transfer",
-                "Top Up", "Top Up", "Menerima", "Menerima", "Top Up",
-                "Transfer", "Transfer" };
-        // Inflate the layout for this fragment
-        final ListView list = (ListView)view.findViewById(R.id.list_view);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, items);
-        list.setAdapter(adapter);
+        listTransaction = (RecyclerView) view.findViewById(R.id.list_item);
+        List<Transaction> dataTransaction = new ArrayList<Transaction>();
+        transactionAdapter = new TransactionAdapter(dataTransaction, this);
 
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        RecyclerView.LayoutManager lm = new LinearLayoutManager(getContext());
+        listTransaction.setLayoutManager(lm);
+        listTransaction.setItemAnimator(new DefaultItemAnimator());
+        listTransaction.setAdapter(transactionAdapter);
 
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-                                    long arg3) {
-                // TODO Auto-generated method stub
-                Log.v("TAG", "CLICKED row number: " + arg2);
-                Intent intent = new Intent(getActivity(), HistoryActivity.class);
-                startActivity(intent);
-            }
+        dataTransaction.add(new Transaction("Setiawan hu", "18-11-2018", "kirim" , "2000"));
+        dataTransaction.add(new Transaction("Erinda Resha", "18-11-2018", "terima", "5000"));
+        dataTransaction.add(new Transaction("Cindy Clara", "18-11-2018", "kirim" , "2000"));
+        dataTransaction.add(new Transaction("Charles Eka", "18-11-2018", "terima", "3000"));
+        dataTransaction.add(new Transaction("Top Up", "18-11-2018", "TopUp", "5000"));
 
-        });
+        transactionAdapter.notifyDataSetChanged();
 
         return view;
     }
+
 }
